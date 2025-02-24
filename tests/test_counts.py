4e371e786ed8e1a27054c2998a853fbaff74f4c0
YongJoo Kim <81239098+All4Nothing@users.noreply.github.com>
2025-02-24 19:42:27 +0900

    rm tests

--- tests/test_counts.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/api/category_counts"  # 정확한 URL 사용
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("API 응답 성공:", response.json())
-else:
-    print("API 응답 실패:", response.status_code)
--- tests/test_services.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from app.services.arxiv_service import fetch_latest_papers
-
-class TestArxivService(unittest.TestCase):
-    def test_fetch_latest_papers(self):
-        papers = fetch_latest_papers()
-        self.assertIsInstance(papers, dict)
-        self.assertIn("status", papers)
